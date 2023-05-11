6d08d3cf7422997f7cc8e4f47597a24c8003dedf
AshishGowtham <122021623+AshishGowtham@users.noreply.github.com>
2023-05-11 12:09:21 +0530

    second commit

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const app= express()
 const port = 7000
 
-const userRoute=require("./routes/user")
-const authRoute=require("./routes/auth")
+// const userRoute=require("./routes/user")
+// const authRoute=require("./routes/auth")
 const dbRoute=require("./database/user-db")
 
-app.use('/user',userRoute)
-app.use('/auth',authRoute)
+// app.use('/user',userRoute)
+// app.use('/auth',authRoute)
 app.use('/user-db',dbRoute)
 
 app.listen(port,()=>{
