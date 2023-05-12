9ba4b963b7573ed35a13c8e9ac9a4bcb934d974c
AshishGowtham <122021623+AshishGowtham@users.noreply.github.com>
2023-05-12 19:20:44 +0530

    first

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const router = express.Router()
 const app= express()
 const port = 7000
 
-// const userRoute=require("./routes/user")
-// const authRoute=require("./routes/auth")
+
 const dbRoute=require("./database/user-db")
 
 // app.use('/user',userRoute)
